965dd50c2cf4451a72e3e822238b45877aefc773
tsharon-byte <sharonova_t@inbox.ru>
2020-04-06 14:43:44 +0300

    Updates

---
+++ precache-manifest.96f209fb04a87f036d3702087830d834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec50fe86c2d4ad81dea1f29fff97402",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1583df1eaa84743b0752",
+    "url": "/calculator/static/css/main.3193156c.chunk.css"
+  },
+  {
+    "revision": "7df7c5a7c37bf1da8bc1",
+    "url": "/calculator/static/js/2.696c1214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.696c1214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1583df1eaa84743b0752",
+    "url": "/calculator/static/js/main.ec06f51e.chunk.js"
+  },
+  {
+    "revision": "548babfea1b067596145",
+    "url": "/calculator/static/js/runtime-main.92c42aea.js"
+  }
+]);
